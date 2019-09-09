5b344f0b7a0390597306c002510806595216a827
Joshua Jones <joshuajones@trentu.ca>
2019-09-09 11:03:49 +0700

    #091002

---
+++ controllers/getTypes.js
@@ -0,0 +1,7 @@
+const Type = require('../models/type')
+
+module.exports = (req, res) => {
+Type.find({}).then(data => {
+	res.send(data)
+})
+}
--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.get('/places/:id', require('./controllers/getPlace'))
 app.patch('/places/:id', require('./controllers/patchPlace'))
 app.delete('/places/:id', require('./controllers/deletePlace'))
 app.post('/type', require('./controllers/postType'))
+app.get('/types', require('./controllers/getTypes'))
 
 
 
