ffcd2354205b9d6a5f90437056daa5e340466b3a
Joshua Jones <joshuajones@trentu.ca>
2019-09-09 11:44:01 +0700

    #091004

---
+++ models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const User = mongoose.model('user',
+	{
+	avatar: {
+		type: String,
+		default: 'https://randomuser.me/api/portraits/lego/1.jpg'
+	},
+	email: {
+		type: String,
+		required: [true, 'email is required']
+	},
+	location: {
+		type: String,
+		required: [true, 'location is required']
+	},
+	name: {
+		type: String,
+		required: [true, 'name is required']
+	},
+	password: {
+		type: String,
+		required: [true, 'password is required'],
+		selected: false
+	},
+})
