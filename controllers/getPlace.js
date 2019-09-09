31bc4986a427e1f479e71ff36f0af380a9901c06
Joshua Jones <joshuajones@trentu.ca>
2019-09-09 12:37:55 +0700

    #091006 Not sure how to query for this, need help

--- controllers/getPlace.js
+++ controllers/getPlace.js
@@ -1,7 +1,7 @@
 const Place = require('../models/place')
 
 module.exports = (req,res) => {
-	Place.findById(req.query).populate('type').populate('host').then(data =>
+	Place.findById(req.query).populate('type').populate({path: 'host', select: 'name avatar'}).then(data =>
 		res.send(data)
 	).catch(err => {
 		console.log(err)
