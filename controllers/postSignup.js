7b46b01bfd828f7c0083c435ee40fd1af2971c3a
Joshua Jones <joshuajones@trentu.ca>
2019-11-06 11:40:58 -0500

    Update postSignup.js

--- controllers/postSignup.js
+++ controllers/postSignup.js
@@ -3,14 +3,12 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 module.exports = (req, res) => {
-	console.log(req.body)
 	req.body.password = bcrypt.hashSync(req.body.password, 10)
-	console.log('>>>>>>>>>>>>>', req.body.password);
 	User.create(req.body)
 		.then(data => {
 			let obj = data.toObject()
 			let token = jwt.sign(obj, process.env.SECRET)
-
+			console.log(token);
 			res.send(token)
 	}).catch (err => {
 		console.log(err)
